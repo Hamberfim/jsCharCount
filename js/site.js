4094d0641dc83c61753f60746e1ac665accd7e33
Anthony <adhamlingithub@gmail.com>
2021-07-30 12:22:20 -0500

    Intial script and html setup

--- js/site.js
+++ js/site.js
@@ -1,6 +1,21 @@
 "use strict";
 
-// call hello World to test Prism Code Display
-function helloWorld() {
-    alert("Test - Hello World!");
+// function to count and display the number of characters
+function displayCount() {
+
+    let userStr = document.getElementById('word').value;
+
+    let count = "Your input has XX characters (not including spaces).";
+    let newUserStr = "";
+    let newCount = "";
+
+    if(count.includes('XX')) {
+        newUserStr = userStr.split(" ").join("");  // remove space to count only letters
+        newCount = count.replace('XX', `${newUserStr.length}`);
+        
+    }
+    let elPhrase = document.getElementById('phrase');
+    elPhrase.textContent = userStr;
+    let elCount = document.getElementById('count');
+    elCount.textContent = newCount;
 }
